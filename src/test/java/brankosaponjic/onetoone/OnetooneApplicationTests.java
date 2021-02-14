6a6c8ff03885984f25371ce7ee9b2ae3decb9222
Branko <brankosaponjic76@gmail.com>
2021-02-14 23:33:52 +0100

    Added testCreateLicense

--- src/test/java/brankosaponjic/onetoone/OnetooneApplicationTests.java
+++ src/test/java/brankosaponjic/onetoone/OnetooneApplicationTests.java
@@ -1,13 +1,35 @@
 package brankosaponjic.onetoone;
 
+import brankosaponjic.onetoone.entity.License;
+import brankosaponjic.onetoone.entity.Person;
+import brankosaponjic.onetoone.repository.LicenseRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 @SpringBootTest
 class OnetooneApplicationTests {
 
+    @Autowired
+    LicenseRepository licenseRepository;
+
     @Test
-    void contextLoads() {
+    void testCreateLicense() {
+        License license = new License();
+        license.setType("CAR");
+        license.setValidFrom(new Date());
+        license.setValidTo(new Date());
+
+        Person person = new Person();
+        person.setFirstName("Bill");
+        person.setLastName("Cristal");
+        person.setAge(55);
+
+        license.setPerson(person);
+
+        licenseRepository.save(license);
     }
 
 }
